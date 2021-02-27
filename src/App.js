8efbf1624868c052685c99610312da86bd39ee7c
shadreza <shadreza100@gmail.com>
2021-02-28 00:25:11 +0600

    Added For lopps functionality via map

--- src/App.js
+++ src/App.js
@@ -8,21 +8,41 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <ComponentName></ComponentName>
+        {productInfo.map(infoObj => <Product nameAndPriceObject ={infoObj} ></Product>)}
         <p>Changed The Emmet Settings</p>
       </header>
     </div>
   );
 }
 
-function ComponentName() {
+const productInfo = [{name:'A',price:10},{name:'B',price:20},{name:'C',price:30}]
+
+function ComponentName(passedParameter) {
   const ComponentStyle = {
     border : '2px solid yellow'
   }
   return (
     <div style={ComponentStyle}>
-      <p>Hi</p>
+      <p>Hi <strong>{passedParameter.name}</strong></p>
       <p>Bye</p>
+    </div>
+  );
+}
+
+function Product (props) {
+  const ProductStyle = {
+    border : '4px solid cyan',
+    marginTop : '20px',
+    padding : '1rem',
+    borderRadius : '20px'
+  }
+  return (
+    <div style={ProductStyle}>
+      <h3>Name : {props.nameAndPriceObject.name}</h3>
+      <p>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, corrupti!</p>
+      <small>Price : <strong>{props.nameAndPriceObject.price}</strong></small>
+      <br/>
+      <button>Buy Now</button>
     </div>
   );
 }
